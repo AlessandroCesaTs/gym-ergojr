3b8fdb9cba8ec4773a5e9c5bdba6c446c6c29231
Alessandro Cesa <125821182+AlessandroCesaTs@users.noreply.github.com>
2024-08-06 11:37:51 +0200

    updated from gym to gymnasium

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='gym_ergojr',
     version='1.3',
     install_requires=[
-        'gym>=0.2.3', 'pybullet>=1.9.4', 'scikit-learn', 'scipy', "tqdm",
+        'gymnasium', 'pybullet>=1.9.4', 'scikit-learn', 'scipy', "tqdm",
         "matplotlib", "numpy"
     ])
